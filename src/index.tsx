1de52516f63fd64b7ac69850352e41035d0246a5
Irene Strigo <irene.strigo@gmail.com>
2024-05-08 18:33:13 +0200

    fix: change styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { getProducts } from './services/Client';
 import 'normalize.css';
+//import RegistrationPage from './pages/registr-page/registr-page';
 //import MainPage from './pages/main-page/main-page';
-//import LoginPage from './pages/login-page/login-page';
-import RegistrationPage from './pages/registr-page/registr-page';
+import LoginPage from './pages/login-page/login-page';
 
 const App = () => {
   // console.log(process.env.CTP_CLIENT_SECRET);
   getProducts().then(console.log);
 
   return (
-    <div>
-      <RegistrationPage></RegistrationPage>
-      Hello React App
+    <div className="app-wrapper">
+      <LoginPage></LoginPage>
     </div>
   );
 };
--- src/pages/registr-page/registr-page.tsx
+++ src/pages/registr-page/registr-page.tsx
@@ -10,7 +10,7 @@ import Input from '../../components/input/input';
 
 const RegistrationPage = (): ReactElement => {
   return (
-    <div className="login-page">
+    <div className="registration-page">
       <Header></Header>
       <PageWrapper
         items={
