00b85567b85a755389a4c147d38f08142394ce67
Hanna Dziakava <anne.dyakova@gmail.com>
2024-05-08 20:33:22 +0200

    feat: implement router

---
+++ src/pages/error-page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Header } from '../components';
+
+const ErrorPage = () => {
+  return (
+    <>
+      <Header />
+      <div>Error Page</div>
+    </>
+  );
+};
+
+export default ErrorPage;
---
+++ src/pages/login-page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Header } from '../components';
+
+const LogInPage = () => {
+  return (
+    <>
+      <Header />
+      <div>Log In Page</div>
+    </>
+  );
+};
+
+export default LogInPage;
