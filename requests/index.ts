ee1f1471f16627f497dc81a1cf6c7469fe706008
Irene Strigo <irene.strigo@gmail.com>
2024-06-02 23:01:26 +0200

    feat: remove unused code

--- requests/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as GetAdminToken } from './GetAdminToken';
-export { default as GetAnonymousToken } from './GetAnonymousToken';
-export { default as GetAccessAndRefreshToken } from './GetAccessAndRefreshToken';
-export { default as SignUpCustomer } from './SignUpCustomer';
-export { default as GetClient } from './GetClient';
-export { default as SignInCustomer } from './SignInCustomer';
-export { default as GetProduct } from './GetProduct';
